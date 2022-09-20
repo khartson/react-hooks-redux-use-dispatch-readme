a8366e7b45594f95ae2aaf9a35b924ba06371f04
Kyle <khartson2017@gmail.com>
2022-09-20 11:01:51 -0400

    commit tree 89c86d22926214e138a9b36dc3531ff90669474b

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux"; /* code change */
-import counterReducer from "./features/counter/counterSlice.js";
+import { counterReducer } from "./features/counter/counterSlice.js";
 import App from "./App";
 import "./index.css";
 
